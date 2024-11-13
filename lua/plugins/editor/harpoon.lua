98853489f64c8357e4b338d6927e5a2c668ea11f
Ameer Arsala <ameer.arsala03@gmail.com>
2024-11-12 16:26:12 -0800

    made harpoon more clear, awaiting my PR to be merged into harpoon2

--- lua/plugins/editor/harpoon.lua
+++ lua/plugins/editor/harpoon.lua
@@ -1,24 +1,13 @@
 return {
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
   {
-    --"benlubas/harpoon", -- fork that caches the git branch key, and adds some highlights and stuff
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    -- config = function()
-    --   --   --local theme = require("benlubas.color")
-    --   --   -- vim.api.nvim_set_hl(0, "HarpoonBorder", theme.fancy_float.border)
-    --   --   -- vim.api.nvim_set_hl(0, "HarpoonWindow", theme.fancy_float.window)
-    --   --   -- vim.api.nvim_set_hl(0, "HarpoonTitle", theme.fancy_float.title)
-    --   --   -- vim.api.nvim_set_hl(0, "HarpoonCurrent", { fg = theme.harpoon_current })
-    --   --
-    --   local harpoon = require("harpoon")
-    --   harpoon:setup()
-    -- end,
     opts = {
       menu = {
         width = (function()
           local width = vim.api.nvim_win_get_width(0) - 4
-          return math.floor(width - width * 0.55)
+          return math.floor(width - (width * 0.55))
         end)(),
       },
       excluded_filetypes = { "harpoon", "starter", "oil", "molten_output" },
@@ -51,7 +40,7 @@ return {
           "<leader>h",
           function()
             local harpoon = require("harpoon")
-            harpoon.ui:toggle_quick_menu(harpoon:list())
+            harpoon.ui:toggle_quick_menu(harpoon:list(), { title = "HARPOON", height_in_lines = 10 })
           end,
           desc = "Harpoon: Quick Menu",
         },
